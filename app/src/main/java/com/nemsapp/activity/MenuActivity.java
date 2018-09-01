f43ac5bfb8474fb64f4609b9ed3b7e6c63e52d41
bcy97 <609233559@qq.com>
2018-09-01 10:53:34 +0800

    add 计算MD5

--- app/src/main/java/com/nemsapp/activity/MenuActivity.java
+++ app/src/main/java/com/nemsapp/activity/MenuActivity.java
@@ -1,19 +1,56 @@
 package com.nemsapp.activity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.gson.Gson;
 import com.nemsapp.R;
+import com.nemsapp.util.Constants;
+import com.nemsapp.util.MD5;
+
+import java.io.File;
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okio.BufferedSink;
+import okio.Okio;
+import okio.Sink;
 
 public class MenuActivity extends AppCompatActivity {
+
+    final OkHttpClient client = new OkHttpClient();
+
+    //读写权限
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE};
+    //请求状态码
+    private static int REQUEST_PERMISSION_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, PERMISSIONS_STORAGE, REQUEST_PERMISSION_CODE);
+            }
+        }
 
         Button button1 = findViewById(R.id.button1);
         Button button2 = findViewById(R.id.button2);
@@ -70,6 +107,8 @@ public class MenuActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        initConfig();
     }
 
     @Override
@@ -86,5 +125,96 @@ public class MenuActivity extends AppCompatActivity {
         intent.addCategory("android.intent.category.DEFAULT");
         intent.addCategory("android.intent.category.MONKEY");
         startActivity(intent);
+    }
+
+    public void initConfig() {
+
+        File folder = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + Constants.folderPath);
+        System.out.println(Environment.getExternalStorageDirectory().getAbsolutePath() + Constants.folderPath);
+
+        if (!folder.exists()) {
+            folder.mkdirs();
+        }
+
+        File configList = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + Constants.folderPath + "/configList.xml");
+
+        if (configList.exists()) {
+            String data = MD5.getMD5(configList);
+            String url = "http://" + Constants.ip + ":8080/file/compare";
+            OkHttpClient okHttpClient = new OkHttpClient();
+            final Request request = new Request.Builder()
+                    .url(url)
+                    .build();
+            final Call call = okHttpClient.newCall(request);
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Response response = call.execute();
+                        Gson gson = new Gson();
+                        boolean result = Boolean.parseBoolean(response.body().string());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+        } else {
+            downloadConfigList();
+        }
+
+    }
+
+    private void downloadConfigList() {
+        //下载路径，如果路径无效了，可换成你的下载路径
+        final String url = "http://" + Constants.ip + ":8080/configList.xml";
+        final long startTime = System.currentTimeMillis();
+        Log.i("DOWNLOAD", "startTime=" + startTime);
+
+        Request request = new Request.Builder().url(url).build();
+        new OkHttpClient().newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                // 下载失败
+                e.printStackTrace();
+                Log.i("DOWNLOAD", "download failed");
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Sink sink = null;
+                BufferedSink bufferedSink = null;
+                try {
+                    String mPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/nemsapp";
+//                    String mPath = Environment.getDataDirectory().getAbsolutePath();
+                    System.out.println(mPath);
+                    File dest = new File(mPath, url.substring(url.lastIndexOf("/") + 1));
+                    sink = Okio.sink(dest);
+                    bufferedSink = Okio.buffer(sink);
+                    bufferedSink.writeAll(response.body().source());
+
+                    bufferedSink.close();
+                    Log.i("DOWNLOAD", "download success");
+                    Log.i("DOWNLOAD", "totalTime=" + (System.currentTimeMillis() - startTime));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Log.i("DOWNLOAD", "download failed");
+                } finally {
+                    if (bufferedSink != null) {
+                        bufferedSink.close();
+                    }
+
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_PERMISSION_CODE) {
+            for (int i = 0; i < permissions.length; i++) {
+                Log.i("MainActivity", "申请的权限为：" + permissions[i] + ",申请结果：" + grantResults[i]);
+            }
+        }
     }
 }
--- app/src/main/java/com/nemsapp/util/Constants.java
+++ app/src/main/java/com/nemsapp/util/Constants.java
@@ -4,4 +4,6 @@ public class Constants {
 
     final static public String ip = "192.168.2.176";
 
+    final static public String folderPath = "/nemsapp";
+
 }
