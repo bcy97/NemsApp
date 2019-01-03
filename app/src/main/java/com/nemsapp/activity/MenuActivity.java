113325c1bbe7d6793e801628322c23d43dc5abd8
bcy97 <609233559@qq.com>
2019-01-04 01:29:49 +0800

    add 菜单界面绘制

--- app/src/main/java/com/nemsapp/activity/MenuActivity.java
+++ app/src/main/java/com/nemsapp/activity/MenuActivity.java
@@ -9,17 +9,37 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.GridView;
+import android.widget.SimpleAdapter;
 
 import com.nemsapp.R;
 import com.nemsapp.util.FileHelper;
+import com.nemsapp.util.GlideImageLoader;
 import com.nemsapp.vo.FileMD5;
+import com.youth.banner.Banner;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    //滚动栏
+    Banner banner;
+    //6格导航栏
+    GridView menu;
+    private List<Map<String, Object>> dataList;
+    private int[] icon = {R.drawable.ic_monitor, R.drawable.ic_graph,
+            R.drawable.ic_data, R.drawable.ic_statistics,
+            R.drawable.ic_alert, R.drawable.ic_search};
+    private String[] iconName = {"监控图", "曲线图", "实时数据", "累加量统计", "重要警报", "查询统计"};
+    private SimpleAdapter adapter;
 
     private FileHelper fileHelper = new FileHelper();
 
@@ -43,59 +63,54 @@ public class MenuActivity extends AppCompatActivity {
             }
         }
 
-        Button button1 = findViewById(R.id.button1);
-        Button button2 = findViewById(R.id.button2);
-        Button button3 = findViewById(R.id.button3);
-        Button button4 = findViewById(R.id.button4);
-        Button button5 = findViewById(R.id.button5);
-        Button button6 = findViewById(R.id.button6);
+        //初始化滚动栏
+        banner = findViewById(R.id.home_banner);
+        //资源文件
+        Integer[] images = {R.drawable.p1, R.drawable.p2, R.drawable.p3, R.drawable.p4};
+        //设置图片加载器
+        banner.setImageLoader(new GlideImageLoader());
+        //设置图片集合
+        banner.setImages(Arrays.asList(images));
+        //banner设置方法全部调用完毕时最后调用
+        banner.start();
 
-        button1.setOnClickListener(new View.OnClickListener() {
+        //初始化导航栏
+        menu = findViewById(R.id.menu);
+        dataList = new ArrayList<>();
+        adapter = new SimpleAdapter(this, gatData(), R.layout.item_menu,
+                new String[]{"pic", "text"}, new int[]{R.id.pic, R.id.text});
+        menu.setAdapter(adapter);
+
+        menu.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MenuActivity.this, MonitorPicActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MenuActivity.this, GraphActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MenuActivity.this, RealDataActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MenuActivity.this, CumulantDataActivity.class);
-                startActivity(intent);
-            }
-        });
-
-
-        button5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MenuActivity.this, AlertActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        button6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MenuActivity.this, StatisActivity.class);
-                startActivity(intent);
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent;
+                switch (position) {
+                    case 0:
+                        intent = new Intent(MenuActivity.this, MonitorPicActivity.class);
+                        startActivity(intent);
+                        break;
+                    case 1:
+                        intent = new Intent(MenuActivity.this, GraphActivity.class);
+                        startActivity(intent);
+                        break;
+                    case 2:
+                        intent = new Intent(MenuActivity.this, RealDataActivity.class);
+                        startActivity(intent);
+                        break;
+                    case 3:
+                        intent = new Intent(MenuActivity.this, CumulantDataActivity.class);
+                        startActivity(intent);
+                        break;
+                    case 4:
+                        intent = new Intent(MenuActivity.this, AlertActivity.class);
+                        startActivity(intent);
+                        break;
+                    case 5:
+                        intent = new Intent(MenuActivity.this, StatisActivity.class);
+                        startActivity(intent);
+                        break;
+                }
             }
         });
 
@@ -103,6 +118,17 @@ public class MenuActivity extends AppCompatActivity {
 
         fileHelper.initPictureLib();
 
+    }
+
+    private List<Map<String, Object>> gatData() {
+        // TODO Auto-generated method stub
+        for (int i = 0; i < icon.length; i++) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("pic", icon[i]);
+            map.put("text", iconName[i]);
+            dataList.add(map);
+        }
+        return dataList;
     }
 
     @Override
@@ -125,5 +151,10 @@ public class MenuActivity extends AppCompatActivity {
                 Log.i("MainActivity", "申请的权限为：" + permissions[i] + ",申请结果：" + grantResults[i]);
             }
         }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
     }
 }
